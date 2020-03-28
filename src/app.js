6f4396ecadf8c1e9f4f78d7edfea59ceab8519d8
Victor Biasibetti <victorbiasibetti7@gmail.com>
2020-03-28 16:56:22 -0300

    configuração da conexão do banco de dados e inserção de um usuário teste

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 class App {
   constructor() {
     this.server = express();
     this.middlewares();
-    this.routes(); 
+    this.routes();
   }
 
   middlewares() {
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'hello world' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Victor',
+    email: 'victorbiasibetti7@gmail.com',
+    password_hash: '123456789',
+  });
+  res.json(user);
 });
 
 export default routes;

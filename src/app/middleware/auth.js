1fafef33f27ae6e50e4bf3d1e244284f6b2d953c
Victor Biasibetti <victorbiasibetti7@gmail.com>
2020-03-31 10:28:38 -0300

    adição de permissão nas rotas

--- src/app/middleware/auth.js
+++ src/app/middleware/auth.js
@@ -2,21 +2,46 @@ import jwt from 'jsonwebtoken';
 import { promisify } from 'util';
 
 import authConfig from '../../config/auth';
+import User from '../models/User';
 
-export default async (req, res, next) => {
-  const authHeader = req.headers.authorization;
+class Auth {
+  async Authorization(req, res, next) {
+    const authHeader = req.headers.authorization;
 
-  if (!authHeader) {
-    return res.status(401).json({ error: 'Token ausente' });
+    if (!authHeader) {
+      return res.status(401).json({ error: 'Token ausente' });
+    }
+
+    const [, token] = authHeader.split(' ');
+
+    try {
+      const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+      req.userId = decoded.id;
+      return next();
+    } catch (err) {
+      return res.status(401).json({ error: 'Token inválido' });
+    }
   }
 
-  const [, token] = authHeader.split(' ');
+  async UserPermission(req, res, next) {
+    const authHeader = req.headers.authorization;
 
-  try {
-    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
-    req.userId = decoded.id;
-    return next();
-  } catch (err) {
-    return res.status(401).json({ error: 'Token inválido' });
+    if (!authHeader) {
+      return res.status(401).json({ error: 'Token ausente' });
+    }
+
+    const [, token] = authHeader.split(' ');
+
+    try {
+      const { id } = await promisify(jwt.verify)(token, authConfig.secret);
+      const user = await User.findOne({ where: { id, admin: true } });
+      if (!user) {
+        return res.status(401).json({ error: 'Usuário sem permissão' });
+      }
+      return next();
+    } catch (err) {
+      return res.status(401).json({ error: 'Token inválido' });
+    }
   }
-};
+}
+export default new Auth();
--- src/routes.js
+++ src/routes.js
@@ -8,45 +8,61 @@ import DepartamentController from './app/controllers/DepartamentController';
 import JobController from './app/controllers/JobController';
 import ReportController from './app/controllers/ReportController';
 
-import authMiddleware from './app/middleware/auth';
+import Auth from './app/middleware/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
-routes.use(authMiddleware);
+routes.use(Auth.Authorization);
 
-routes.put('/users', UserController.update);
-routes.delete('/users', UserController.delete);
-routes.get('/users', UserController.index);
+routes.put('/users', Auth.UserPermission, UserController.update);
+routes.delete('/users', Auth.UserPermission, UserController.delete);
+routes.get('/users', Auth.UserPermission, UserController.index);
 
 routes.get('/tasks', TaskController.index);
 routes.post('/tasks', TaskController.store);
 routes.put('/tasks', TaskController.update);
-routes.delete('/tasks', TaskController.delete);
+routes.delete('/tasks', Auth.UserPermission, TaskController.delete);
 
-routes.get('/tasks-status', TaskStatusController.index);
-routes.post('/tasks-status', TaskStatusController.store);
-routes.put('/tasks-status', TaskStatusController.update);
-routes.delete('/tasks-status', TaskStatusController.delete);
+routes.get('/tasks-status', Auth.UserPermission, TaskStatusController.index);
+routes.post('/tasks-status', Auth.UserPermission, TaskStatusController.store);
+routes.put('/tasks-status', Auth.UserPermission, TaskStatusController.update);
+routes.delete(
+  '/tasks-status',
+  Auth.UserPermission,
+  TaskStatusController.delete
+);
 
-routes.get('/tasks-type', TaskTypeController.index);
-routes.post('/tasks-type', TaskTypeController.store);
-routes.put('/tasks-type', TaskTypeController.update);
-routes.delete('/tasks-type', TaskTypeController.delete);
+routes.get('/tasks-type', Auth.UserPermission, TaskTypeController.index);
+routes.post('/tasks-type', Auth.UserPermission, TaskTypeController.store);
+routes.put('/tasks-type', Auth.UserPermission, TaskTypeController.update);
+routes.delete('/tasks-type', Auth.UserPermission, TaskTypeController.delete);
 
-routes.get('/departament', DepartamentController.index);
-routes.post('/departament', DepartamentController.store);
-routes.put('/departament', DepartamentController.update);
-routes.delete('/departament', DepartamentController.delete);
+routes.get('/departament', Auth.UserPermission, DepartamentController.index);
+routes.post('/departament', Auth.UserPermission, DepartamentController.store);
+routes.put('/departament', Auth.UserPermission, DepartamentController.update);
+routes.delete(
+  '/departament',
+  Auth.UserPermission,
+  DepartamentController.delete
+);
 
 routes.get('/job', JobController.index);
 routes.post('/job', JobController.store);
 routes.put('/job', JobController.update);
-routes.delete('/job', JobController.delete);
+routes.delete('/job', Auth.UserPermission, JobController.delete);
 
-routes.get('/report', ReportController.CountCompletedTasks);
-routes.get('/report/time', ReportController.TimeOfClosionTasks);
+routes.get(
+  '/report',
+  Auth.UserPermission,
+  ReportController.CountCompletedTasks
+);
+routes.get(
+  '/report/time',
+  Auth.UserPermission,
+  ReportController.TimeOfClosionTasks
+);
 
 export default routes;
